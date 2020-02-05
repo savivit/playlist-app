89eadab7c0468d3f3156460974c6e0531b12f466
savivit <savivit@gmail.com>
2020-02-05 13:15:01 +0200

    Updates

---
+++ precache-manifest.7e1780871c3087d0d489f2642862b307.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb374bc54a49dc175f25a36a18d4cd32",
+    "url": "/playlist-app/index.html"
+  },
+  {
+    "revision": "4bfd852296d4320c1a9a",
+    "url": "/playlist-app/static/css/main.dcc5d8d5.chunk.css"
+  },
+  {
+    "revision": "6cc4b73a9908286100ca",
+    "url": "/playlist-app/static/js/2.50c4a24e.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/playlist-app/static/js/2.50c4a24e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4bfd852296d4320c1a9a",
+    "url": "/playlist-app/static/js/main.59a58d3b.chunk.js"
+  },
+  {
+    "revision": "bc35f55a8adf76243464",
+    "url": "/playlist-app/static/js/runtime-main.ac58e9b5.js"
+  },
+  {
+    "revision": "74942900b307a0671ec44fcc7d3a0a53",
+    "url": "/playlist-app/static/media/piano_light_rgb_fill.74942900.png"
+  }
+]);
